8f01070decffc690b7bb370b893f590b8bd9af42
Trang Nguyen <Trang.Nguyen@dematic.com>
2017-07-06 10:19:39 -0500

    added linear regression example

--- regression.py
+++ regression.py
@@ -5,16 +5,20 @@ from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
+# read in data
 df = quandl.get('WIKI/GOOGL')
 # print(df.columns)
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 # print(df.columns)
 
+# add more columns
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
+# take what we need
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
@@ -35,18 +39,27 @@ df.dropna(inplace=True)
 
 y = np.array(df['label'])
 
+# cross validation
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
+'''
 clf = LinearRegression(n_jobs=-1)
 # clf = svm.SVR(kernel='poly')
 clf.fit(X_train, y_train)
 
+with open('linearregression.pickle','wb') as f:
+    pickle.dump(clf, f)
+'''
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
 
 forecast_set = clf.predict(X_lately)
-
 print(forecast_set, accuracy, forecast_out)
 
+# create chart
 df['Forecast'] = np.nan
 
 last_date = df.iloc[-1].name
